0d998032f36f77180c5e534e2bac93d446b477e8
jailingeshwarR <jailingeshwar.ravi@increscotech.com>
2024-10-11 11:29:43 +0530

    add packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,16 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ExpenseModule } from './expense/expense.module';
+import { AuthModule } from './auth/auth.module';
+import { AssignmentsModule } from './assignments/assignments.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://sully6458:PLaFCPjKkKC8hu6a@cluster0.b65i5cz.mongodb.net/test?retryWrites=true&w=majority'),
     UserModule,
-    ExpenseModule
+    ExpenseModule,
+    AuthModule,
+    AssignmentsModule
   ],
   controllers: [AppController],
   providers: [AppService],
